30ad0da4da1d4e6408b8896882aa72cfa7b20299
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2023-11-23 21:05:20 +0700

    feat: add nav view

--- app/views/partials/nav.view.php
+++ app/views/partials/nav.view.php
@@ -1,8 +1,19 @@
-<nav>
-	<ul>
-		<li><a href="<?php echo App::get('root_uri'); ?>" title="Home">Home</a></li>
-		<li><a href="about" title="About us">About us</a></li>
-		<li><a href="contact" title="Your feedback is important to us">Contact</a></li>
-		<li><a href="users" title="All Users">View Users</a></li>
-	</ul>
+<nav class="navbar navbar-expand-lg sticky-top bg-white">
+	<div class="container-fluid px-5 py-2">
+		<a class="navbar-brand" href="#">Tertib APP</a>
+		<button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a href="<?php echo App::get('root_uri'); ?>" title="Home" class="nav-link active">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="about">About</a>
+				</li>
+			</ul>
+			<a class="btn border-0 rounded-3 fs-6 my-auto" href="login" style="width:min(50%,155px); background-color:var(--orange); color:var(--white);">Login</a>
+		</div>
+	</div>
 </nav>
