3b000e059effe20274cdd750f9a5acb7023ef45a
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2023-11-23 21:03:53 +0700

    feat: add landing page view

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,49 +1,91 @@
-<!DOCTYPE html>
-<html>
+<?php require 'partials/nav.view.php'; ?>
 
-<head>
-	<title>Learn | PHP</title>
-	<style>
-		html {
-			font-family: Lato;
-		}
 
-		header {
-			padding: 2em;
-			background-color: #e3e3e3;
-			text-align: center;
-		}
+<main class='container'>
+	<!-- Start Hero Section -->
+	<section class="row justify-content-between" style='height:80vh;'>
+		<div class="col flex-column align-self-center">
+			<h1 class="lh-1" style="font-family:Poppins Extra Bold;font-size:5em;">WELCOME TO JTI.TERTIB</h1>
+			<div class="d-flex gap-2" style="font-family:Poppins Medium; color:var(--orange-opa)">
+				<h6>#JTIdisiplin</h6>
+				<h6>#JTItertib</h6>
+			</div>
+			<p class="py-3" style="font-family:Poppins Medium;width:max(50%,350px)">Laporkan Para Oknum Kampus yang
+				Kurang Tertib dan Disiplin di <span style="color:var(--yellow);">Tertib</span> App</p>
+			<!-- link Report Button -->
+			<a href="report" class="py-2 px-5 rounded-3 link-underline link-underline-opacity-0" style="color:var(--white);font-family:Poppins Semi Bold; background:var(--btn-gradient);">Report ></a>
+		</div>
+		<div class="col ">
+			<div class="container position-relative end-0">
+				<div class="cirle rounded-circle position-absolute end-0" style='width: 463px;height: 463px;background: linear-gradient(180deg, #FCB216 6.77%, #F3542C 27.6%, rgba(255, 255, 255, 0.32) 65.62%);'></div>
+				<img class="position-absolute end-0" src="<?php echo App::get("root_uri") . "/public/img/code of conduct image.png" ?>" alt="code of conduct image">
+			</div>
+		</div>
+	</section>
+	<!-- End Hero Section -->
 
-		input[type="text"] {
-			border: 1px solid #5AB0F3;
-			border-radius: 5px;
-			height: 30px;
-		}
 
-		input[type="text"]:focus {
-			border-radius: 5px;
-		}
+	<!-- Start Tingkat Pelanggaran Section -->
+	<section class="py-5">
+		<div class="container">
+			<div class="col">
+				<div class="row align-items-center text-center pb-5">
+					<h2>Tingkat Pelanggaran</h2>
+					<p>Kenali tingkat pelanggaran Mahasiswa</p>
+				</div>
+				<div class="row gap-5">
+					<?php
+					// color for border
+					$color = array('dark-blue', 'red', 'grey', 'orange-opa', 'yellow');
+					?>
+					<div class="col">
+						<div class="card border-0 align-items-center bg-transparent">
+							<div class="card-emblem border border-2 rounded-circle p-2" style="height:100px;width:100px;border-color:var(--dark-blue) !important;"> <!--add color in var for changes color-->
+								<h1 class="text-center rounded-circle p-4 p-md-3" style="background-color:var(--white-gray); filter:var(--drop-shadow)">I</h1> <!--for number -->
+							</div>
+							<div class="card-body text-center" style="width:min(80%,200px);">
+								<h5 class="card-title">Pelanggaran Sangat Berat</h5> <!--for text -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>
+	<!-- End Tingkat Pelanggaran Section -->
 
-		.btn {
-			border: 1px solid #e65100;
-			border-radius: 0px;
-			font-size: 30px;
-			background-color: #ffffff;
-			cursor: pointer;
-			color: #e65100;
-			font-family: Lato, Helvetica;
-		}
+	<!-- Start Table Tatib -->
+	<section class="py-5">
+		<div class="container">
+			<div class="col">
+				<div class="row align-items-center justify-content-center text-center pb-5">
+					<h2 style="width:min(80%,420px);">Klasifikasi Pelanggaran Tata Tertib Mahasiswa</h2>
+				</div>
+				<div class="row table-responsive">
+					<table class="table table-bordered">
+						<thead class="table-light">
+							<tr>
+								<th scope="col">No</th>
+								<th scope="col">Pelanggaran</th>
+								<th scope="col">Tingkat</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
 
-		.btn:hover {
-			background-color: #e65100;
-			color: white;
-		}
-	</style>
-</head>
+								<th scope="row">1</th> <!--for number-->
+								<td>Mark Lorem ipsum dolor sit.</td> <!--for text pelanggaran-->
+								<td>Otto</td> <!--for Tingkat-->
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Table Tatib -->
 
-<body>
-	<?php require 'partials/nav.view.php'; ?>
-	<h1>HomePage</h1>
-</body>
+	<footer class="text-center p-2 " style="font-size: 12px;">
+		<p>Copyright | Privacy Policy</p>
+	</footer>
 
-</html>
+</main>
