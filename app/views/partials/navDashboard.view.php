b61617a504cbc3c3a2d41f377a1e9c493ce398b7
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2023-11-25 11:23:56 +0700

    feat: add navbar for dashboard view

---
+++ app/views/partials/navDashboard.view.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid align-items-lg-start flex-row-reverse flex-lg-column">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav mb-2 mb-lg-0 flex-column align-items-end  align-items-lg-start flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Report</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Chat</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Notification</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                    <li class="nav-item logOut">
+                        <a href="logout" class="nav-link">Log Out</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+        <a class="navbar-brand" href="#">TATIB</a>
+</nav>
