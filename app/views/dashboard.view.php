664e9d180102170d0bae0b7a606e3ec325765080
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2023-11-25 11:20:31 +0700

    feat: add dashboard view

---
+++ app/views/dashboard.view.php
@@ -0,0 +1,15 @@
+<div class="container-fluid ">
+    <div class="row flex-column flex-lg-row">
+        <div class="col-auto">
+            <?php require 'partials/navDashboard.view.php';
+            ?>
+        </div>
+
+        <main class="col ">
+            <div class="container-fluid">
+                Hello world
+
+            </div>
+        </main>
+    </div>
+</div>
