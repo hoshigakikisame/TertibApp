25eab3b5455131de4fb53feff8248a106df5b08c
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2023-11-26 15:29:37 +0700

    feat: add icon and user information

--- app/views/partials/navDashboard.view.php
+++ app/views/partials/navDashboard.view.php
@@ -1,36 +1,51 @@
-<nav class="navbar navbar-expand-lg">
-    <div class="container-fluid align-items-lg-start flex-row-reverse flex-lg-column">
+<nav class="navbar navbar-expand-lg align-items-stretch" style="">
+    <div class="container-fluid align-items-lg-start flex-lg-column ">
+        <a class="navbar-brand my-3" href="#">LOGO TATIB</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarOffcanvasLg" aria-controls="navbarOffcanvasLg">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel">
+
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarOffcanvasLg" aria-labelledby="navbarOffcanvasLgLabel" style="">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
-            <div class="offcanvas-body">
+            <div class="offcanvas-body flex-column " style="height:80vh">
                 <ul class="navbar-nav mb-2 mb-lg-0 flex-column align-items-end  align-items-lg-start flex-grow-1 pe-3">
-                    <li class="nav-item">
+                    <li class="nav-item d-flex align-items-center mb-2 gap-1">
+                        <i class="bi bi-house"></i>
                         <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item d-flex align-items-center mb-2 gap-1">
+                        <i class="bi bi-exclamation-circle"></i>
                         <a class="nav-link" href="#">Report</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Chat</a>
-                    </li>
-                    <li class="nav-item">
+                    <li class="nav-item d-flex align-items-center mb-2 gap-1">
+                        <i class="bi bi-bell"></i>
                         <a class="nav-link" href="#">Notification</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item d-flex align-items-center mb-2 gap-1">
+                        <i class="bi bi-person"></i>
                         <a class="nav-link" href="#">Profile</a>
                     </li>
-                    <li class="nav-item logOut">
+                    <li class="nav-item logOut d-flex align-items-center border-top mt-4 gap-2">
+                        <i class="bi bi-box-arrow-in-right"></i>
                         <a href="logout" class="nav-link">Log Out</a>
                     </li>
                 </ul>
-
+                <div class="container end-0">
+                    <div class="card-user">
+                        <img src="" alt="">
+                        <div class="userinfo">
+                            <h3 style="margin-bottom:-5px">Jonas sutoyo</h3>
+                            <p>Dosen</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <a class="navbar-brand" href="#">TATIB</a>
 </nav>
+
+<script>
+
+</script>
