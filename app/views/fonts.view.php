c9f3fc772b6a9f26049a648849cea06e6b398c2c
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2023-12-05 14:10:07 +0700

    feat: refactor base view and index view

---
+++ app/views/fonts.view.php
@@ -0,0 +1,51 @@
+<style>
+    /* start font initialize */
+    @font-face {
+        font-family: "Poppins-Extra-Light";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-ExtraLight.ttf" ?>") format('truetype');
+    }
+
+    @font-face {
+        font-family: "Poppins-Light";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-Light.ttf" ?>") format('truetype');
+    }
+
+    @font-face {
+        font-family: "Poppins-Thin";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-Thin.ttf" ?>") format('truetype');
+    }
+
+    @font-face {
+        font-family: "Poppins-Regular";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-Regular.ttf" ?>") format('truetype');
+    }
+
+    @font-face {
+        font-family: "Poppins-Medium";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-Medium.ttf" ?>") format('truetype');
+    }
+
+    @font-face {
+        font-family: "Poppins-Semi-Bold";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-SemiBold.ttf" ?>") format('truetype');
+    }
+
+    @font-face {
+        font-family: "Poppins-Bold";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-Bold.ttf" ?>") format('truetype');
+    }
+
+    @font-face {
+        font-family: "Poppins-Extra-Bold";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-ExtraBold.ttf" ?>") format('truetype');
+    }
+
+    @font-face {
+        font-family: "Poppins-Black";
+        src: url("<?php echo App::get("root_uri") . "/public/font/Poppins/Poppins-Black.ttf" ?>") format('truetype');
+    }
+
+
+
+    /* end font initialize */
+</style>
