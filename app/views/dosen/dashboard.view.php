9ae1ec6588a1a559bb59be10e365e220a27a3e0b
unknown <angelicacoustarika@gmail.com>
2023-12-09 17:51:50 +0700

    Style: Update dashboard box colour

--- app/views/dosen/dashboard.view.php
+++ app/views/dosen/dashboard.view.php
@@ -26,14 +26,14 @@
                         </div>
                         <div class="col-lg-2 col-auto border border-2 py-2 rounded-3 flex-grow-1 flex-lg-grow-0">
                             <div class="shadow-sm rounded-3 py-3 px-4 h-100 ">
-                                <h1 class="mb-0">65</h1>
-                                <h6>Checked <br> Reports</h6>
+                                <h1 class="mb-0" style="color:#28A745;">65</h1>
+                                <h6>Valid <br> Reports</h6>
                             </div>
                         </div>
                         <div class="col-lg-2 col-auto border border-2 py-2 rounded-3 flex-grow-1 flex-lg-grow-0">
                             <div class="shadow-sm rounded-3 py-3 px-4 h-100 ">
-                                <h1 class="mb-0" style="">65</h1>
-                                <h6 class="">Unchecked <br> Reports</h6>
+                                <h1 class="mb-0" style=" color:#FF0018;">65</h1>
+                                <h6 class="">Invalid <br> Reports</h6>
                             </div>
                         </div>
 
@@ -49,10 +49,12 @@
                                         <span>des 24</span>
                                     </div>
                                     <h6 class="mb-0"><span class="text-primary">LG7164</span> Deny Prihantoro</h6>
-                                    <p>Melanggar tata tertib berupa tidak memakai pakaian berkerah disaat jam pelajaran dimulai</p>
+                                    <p>Melanggar tata tertib berupa tidak memakai pakaian berkerah disaat jam pelajaran
+                                        dimulai</p>
                                 </div>
                                 <div class="col d-flex justify-content-end">
-                                    <a class="btn btn-primary text-white justify-self-end" href="#" role="button">Report Detail</a>
+                                    <a class="btn btn-primary text-white justify-self-end" href="#" role="button">Report
+                                        Detail</a>
                                 </div>
                             </div>
                         </div>
