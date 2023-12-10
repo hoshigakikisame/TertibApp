1f4503bf94c275c8218baec0232c05991926586c
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2023-12-10 20:14:07 +0700

    Feat: add error page 404 views, and refactor layout 403

--- app/views/error/404.view.php
+++ app/views/error/404.view.php
@@ -2,9 +2,9 @@
     <div class="row justify-content-center align-items-center" style="height: 100vh;">
         <div class="col-lg-6 col-md-8 col-sm-10 col-12">
             <div class="">
-                <h1 class="text-center display-1 text-secondary">4<i class="bi bi-shield-lock"></i>3</h1>
-                <h3 class="text-center">Access Forbidden</h3>
-                <p class="text-center">You don't have permission to access this page</p>
+                <h1 class="text-center display-1 text-secondary">4<i class="bi bi-file-excel"></i>4</h1>
+                <h3 class="text-center">Page Not Found</h3>
+                <p class="text-center">The page you are looking for not available!</p>
                 <div class="text-center">
                     <a href="<?= App::get('root_uri') ?>" class="btn btn-primary">Back to Home</a>
                 </div>
