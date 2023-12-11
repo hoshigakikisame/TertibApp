5d8110683485f13c95a2e5e0d8dd35964be10108
thoriq fathurrozi <thoriqfathurrozi@gmail.com>
2023-12-11 10:06:04 +0700

    Fix: routing paths for MahasiswaController

--- app/routes/mahasiswa_routes.php
+++ app/routes/mahasiswa_routes.php
@@ -1,9 +1,9 @@
 <?php
 
 $router->get('/^mahasiswa\/dashboard$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'MahasiswaController@dashboardPage']);
-$router->get('/^mahasiswa\/profile$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'mahasiswaController@profilePage']);
-$router->post('/^mahasiswa\/profile\/update$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'mahasiswaController@updateProfile']);
-$router->get('/^mahasiswa\/report$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'mahasiswaController@reportPage']);
-$router->post('/^mahasiswa\/report\/new$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'mahasiswaController@addNewReport']);
+$router->get('/^mahasiswa\/profile$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'MahasiswaController@profilePage']);
+$router->post('/^mahasiswa\/profile\/update$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'MahasiswaController@updateProfile']);
+$router->get('/^mahasiswa\/report$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'MahasiswaController@reportPage']);
+$router->post('/^mahasiswa\/report\/new$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'MahasiswaController@addNewReport']);
 
-$router->get('/^mahasiswa\/notification$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'mahasiswaController@notificationPage']);
+$router->get('/^mahasiswa\/notification$/', ['AuthMiddleware@shouldValidated', 'RoleMiddleware@shouldMahasiswa', 'MahasiswaController@notificationPage']);
