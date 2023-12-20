fb000181ef19aadae916aacd7d37e866ff8ebffa
unknown <angelicacoustarika@gmail.com>
2023-12-20 17:25:22 +0700

    Add Href link instagram and website

--- app/views/global/about.view.php
+++ app/views/global/about.view.php
@@ -1,47 +1,58 @@
 <?php Helper::importView('partials/nav.view.php'); ?>
 <main class="container ">
 
-	<div class="row text-center gap-4 justify-content-center position-relative overflow-x-hidden">
-		<h1>Tentang Kami</h1>
-		<div class="col-lg-6 col justify-content-center">
-			<p class="fw-semibold"><span class="fw-bold">Tertib APP </span>merupakan platform pelaporan pelanggaran tata tertib di lingkungan kampus khususnya Jurusan Teknologi Informasi,<br>Politeknik Negeri Malang. </p>
-		</div>
-		<div class="row gy-3 justify-content-evenly position-relative flex-column flex-lg-row">
-			<div class="col-lg-4 col text-start slide-in-left">
-				<h3 class="mb-3">Tujuan Tertib APP</h3>
-				<p class="fs-6" style="text-align:justify;">Dengan semangat keadilan dan keamanan sebagai landasan utama, <span class="fw-bold"> Tertib APP </span> dirancang untuk memberikan solusi efisien dalam pelaporan pelanggaran tata tertib. Kami percaya bahwa keterlibatan aktif seluruh elemen yang ada akan sangat membantu dalam pemantauan tata tertib dapat membentuk lingkungan kampus yang lebih aman dan harmonis.</p>
-			</div>
-			<div class="col-lg-4 col text-start slide-in-right">
-				<h3 class="mb-3">
-					Manfaat Tertib APP
-				</h3>
-				<ul class="list-group list-group-flush">
-					<li class="list-group-item d-flex gap-2 border-0 border py-0">
-						<i class="bi bi-check2-all"></i>
-						<p class="mb-1">Pelaporan Pelanggaran yang Mudah</p>
-					</li>
-					<li class="list-group-item d-flex gap-2 border-0 border py-0">
-						<i class="bi bi-check2-all"></i>
-						<p class="mb-1">Keamanan dan Transparansi</p>
-					</li>
-					<li class="list-group-item d-flex gap-2 py-0">
-						<i class="bi bi-check2-all"></i>
-						<p class="mb-1">Saluran Komunikasi yang Efektif</p>
-					</li>
-				</ul>
-			</div>
-		</div>
-		<div class="col-lg-7 col m-4 ">Mari menjadi bagian dari upaya kolektif kami dalam menciptakan lingkungan belajar yang kondusif dan aman. Dengan mengikuti tata tertib kampus, Anda turut berkontribusi pada keberhasilan dan kesejahteraan bersama.</div>
-		<div class="row justify-content-center mb-4">
-			<div class="col-auto">
-				<i class="bi bi-instagram display-6"></i>
-			</div>
-			<div class="col-auto">
-				<i class="bi bi-globe display-6"></i>
-			</div>
-		</div>
-	</div>
-	<footer class="text-center p-2 " style="font-size: 12px;">
-		<p>Copyright | Privacy Policy</p>
-	</footer>
+    <div class="row text-center gap-4 justify-content-center position-relative overflow-x-hidden">
+        <h1>Tentang Kami</h1>
+        <div class="col-lg-6 col justify-content-center">
+            <p class="fw-semibold"><span class="fw-bold">TertibApp </span>merupakan platform pelaporan pelanggaran tata
+                tertib di lingkungan kampus khususnya Jurusan Teknologi Informasi, Politeknik Negeri Malang. </p>
+        </div>
+        <div class="row gy-3 justify-content-evenly position-relative flex-column flex-lg-row">
+            <div class="col-lg-4 col text-start slide-in-left">
+                <h3 class="mb-3">Tujuan Tertib App</h3>
+                <p class="fs-6" style="text-align:justify;">Dengan semangat keadilan dan keamanan sebagai landasan
+                    utama, <span class="fw-bold"> TertibApp </span> dirancang untuk memberikan solusi efisien dalam
+                    pelaporan pelanggaran tata tertib. Kami percaya bahwa keterlibatan aktif seluruh elemen yang ada
+                    akan sangat membantu dalam pemantauan tata tertib dapat membentuk lingkungan kampus yang lebih aman
+                    dan harmonis.</p>
+            </div>
+            <div class="col-lg-4 col text-start slide-in-right">
+                <h3 class="mb-3">
+                    Manfaat Tertib App
+                </h3>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item d-flex gap-2 border-0 border py-0">
+                        <i class="bi bi-check2-all"></i>
+                        <p class="mb-1">Pelaporan Pelanggaran yang Mudah</p>
+                    </li>
+                    <li class="list-group-item d-flex gap-2 border-0 border py-0">
+                        <i class="bi bi-check2-all"></i>
+                        <p class="mb-1">Keamanan dan Transparansi</p>
+                    </li>
+                    <li class="list-group-item d-flex gap-2 py-0">
+                        <i class="bi bi-check2-all"></i>
+                        <p class="mb-1">Saluran Komunikasi yang Efektif</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-lg-7 col m-4 ">Mari menjadi bagian dari upaya kolektif kami dalam menciptakan lingkungan belajar
+            yang kondusif dan aman. Dengan mengikuti tata tertib kampus, Anda turut berkontribusi pada keberhasilan dan
+            kesejahteraan bersama.</div>
+        <div class="row justify-content-center mb-4">
+            <div class="col-auto">
+                <a class="nav-link" href="https://www.instagram.com/jtipolinema/">
+                    <i class="bi bi-instagram display-6"></i>
+                </a>
+            </div>
+            <div class="col-auto">
+                <a class="nav-link" href="https://jti.polinema.ac.id/">
+                    <i class="bi bi-globe display-6"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center p-2 " style="font-size: 12px;">
+        <p>Copyright | Privacy Policy</p>
+    </footer>
 </main>
