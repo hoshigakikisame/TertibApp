41f965cae8091c801116d698b63f4f251130d411
Rian <rianatgithub@gmail.com>
2023-12-20 15:21:47 +0700

    fix(middleware): Logical error on shouldAnonymous middleware

--- app/middlewares/AuthMiddleware.php
+++ app/middlewares/AuthMiddleware.php
@@ -14,8 +14,27 @@
 
         public function shouldAnonymous() {
             if (Session::getInstance()->has('user')) {
-                Helper::redirect('/auth/dashboard');
-                exit;
+                /**
+                 * @var UserModel $user
+                 */
+                $user = Session::getInstance()->get('user');
+
+                echo $user->getRole();
+
+                switch($user->getRole()) {
+                    case "mahasiswa":
+                        Helper::redirect('/mahasiswa/dashboard');
+                        exit;
+                    case "dosen":
+                        Helper::redirect('/dosen/dashboard');
+                        exit;
+                    case "admin":
+                        Helper::redirect('/admin/dashboard');
+                        exit;
+                    default:
+                        Helper::redirect('/');
+                        exit;
+                }
             }
         }
     }
